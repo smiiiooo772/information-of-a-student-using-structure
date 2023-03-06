76ff82395a2f9442927814a73d4e2228f3bde5ce
smita <116358933+smiiiooo772@users.noreply.github.com>
2023-03-06 18:07:52 +0530

    Add files via upload

---
+++ struct student.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+struct bsc2022
+{
+	int roll;
+	float marks;
+	float 
+	cf;
+}suprita,sunaina;
+int main()
+{
+  suprita.roll=41;
+  suprita.marks=60.50;
+  suprita.cf=999.98;
+  sunaina.roll=10;
+  sunaina.marks=50.45;
+  sunaina.cf=999.98;
+  
+  printf("the roll no of suprita is %d\n",suprita.roll);
+  printf("the marks of suprita is %f\n",suprita.marks);
+  printf("the cf of suprita is %f\n",suprita.cf);
+  printf("the roll no of suprita is %d\n",sunaina.roll);
+  printf("the marks of suprita is %d\n",sunaina.marks);
+  printf("the cf of suprita is %d\n",sunaina.cf);
+  return 0;
+}
+
